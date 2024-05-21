61629d77ab4f6b23a4ef3cc3243da649b03bb0b0
JokizXD <jghgamer22@gmail.com>
2024-05-21 17:10:58 -0400

    add Bomb class

---
+++ js/classes/Bomb.js
@@ -0,0 +1,25 @@
+class Bomb {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.radius = 0;
+    this.color = " ";
+    this.opacity = 1;
+    this.active = false;
+
+    gsap.to(this, {
+      radius: 30
+    });
+  }
+ 
+  draw() {
+    c.save();
+    c.globalAlpha = this.opacity;
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    c.closePath();
+    c.fillStyle = this.color;
+    c.fill();
+    c.restore();
+  }
+}
